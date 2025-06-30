2f1e8202f63588f86a7bc2e4fcf8f8f4c7b06f42
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2025-06-30 22:25:03 +0200

    REST API client is not working.

--- httpx/main.py
+++ httpx/main.py
@@ -1,4 +1,23 @@
+#
+# Copyright 2025 Jaroslav Chmurny
+#
+# This file is part of Python Samples.
+#
+# Python Samples is free software. It is licensed under the Apache License,
+# Version 2.0 # (the "License"); you may not use this file except
+# in compliance with the # License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 from dataclasses import dataclass
+from pprint import pprint
 from typing import List
 
 import httpx
@@ -98,13 +117,13 @@ class RestApiClient:
         if response.status_code != 200:
             raise RestApiError(f"Failed to fetch users: {response.status_code}")
         response_data = response.json()
-        [for user_data in response_data]
-        return []
-
+        return [UserSchema().load(user_data) for user_data in response_data]
 
 
 def main() -> None:
-    pass
+    rest_api_client = RestApiClient()
+    users = rest_api_client.get_users()
+    pprint(users)
 
 
 if __name__ == "__main__":
