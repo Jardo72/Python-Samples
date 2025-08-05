e5209a37ab00d497397c044cc4eee2cb77b4dc55
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2025-08-05 13:24:09 +0200

    parallel-chown-chmod.py rework.

--- shutil/parallel-chown-chmod.py
+++ shutil/parallel-chown-chmod.py
@@ -16,14 +16,13 @@
 # limitations under the License.
 #
 
+from __future__ import annotations
 from argparse import ArgumentParser, RawTextHelpFormatter
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
 from dataclasses import dataclass
 from datetime import datetime
-from grp import getgrnam
-from os import chmod, cpu_count, walk
+from os import chmod, cpu_count, scandir
 from pathlib import Path
-from pwd import getpwnam
 from re import match
 from shutil import chown
 from traceback import print_exc
@@ -41,6 +40,7 @@ class Configuration:
     group: str
     permissions: int
     workers: int
+    dry_run: bool
 
 
 @dataclass(frozen=True)
@@ -72,6 +72,76 @@ class Summary:
     exception_count: int
 
 
+class Prcessor:
+    def __init__(self, config: Configuration):
+        self._config = config
+        self._executor = ProcessPoolExecutor(max_workers=config.workers)
+        self._sequence = Sequence()
+        self._future_list = []
+
+    def _scan_directory(self, path: str) -> None:
+        directories = []
+        files = []
+        with scandir(path) as entries:
+            for entry in entries:
+                if entry.is_dir():
+                    directories.append(entry.path)
+                elif entry.is_file():
+                    files.append(entry.path)
+            request = Request(
+                seq_no=self._sequence.next_value(),
+                path=path,
+                files=tuple(files),
+                user=self._config.user,
+                group=self._config.group,
+                permissions=self._config.permissions,
+            )
+            future = self._executor.submit(process_request, request, self._config.dry_run)
+            self._future_list.append(future)
+        for dir in directories:
+            self._scan_directory(dir)
+
+    def _collect_results(self) -> Summary:
+        exception_count = 0
+        modified_file_count = 0
+        failed_file_count = 0
+        modified_dir_count = 0
+        failed_dir_count = 0
+        for future in self._future_list:
+            try:
+                result = future.result()
+                modified_file_count += result.modified_file_count
+                failed_file_count += result.failed_file_count
+                modified_dir_count += result.modified_dir_count
+                failed_dir_count += result.failed_dir_count
+            except Exception as e:
+                print(f"Error in processing: {e}")
+                exception_count += 1
+
+        return Summary(
+            overall_duration_millis=self._stopwatch.elapsed_time_millis(),
+            modified_file_count=modified_file_count,
+            failed_file_count=failed_file_count,
+            modified_dir_count=modified_dir_count,
+            failed_dir_count=failed_dir_count,
+            exception_count=exception_count,
+        )
+
+    def process(self) -> Summary:
+        self._stopwatch = Stopwatch.start()
+        self._scan_directory(self._config.root_path)
+        return self._collect_results()
+
+
+    def __enter__(self) -> Processor:
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        if self._executor:
+            self._executor.shutdown(wait=True)
+        self._executor = None
+    
+
 def epilog() -> str:
     return """
 This script modifies the ownership and the permissions of the files and directories within
@@ -82,18 +152,26 @@ user and group are applied to all files and directories.
 
 def is_valid_user(user_name: str) -> bool:
     try:
+        from pwd import getpwnam
         getpwnam(user_name)
         return True
     except KeyError:
         return False
+    except ModuleNotFoundError:
+        print("The 'pwd' module is not available on this platform. User validation will be skipped.")
+        return True
     
 
 def is_valid_group(group_name: str) -> bool:
     try:
+        from grp import getgrnam
         getgrnam(group_name)
         return True
     except KeyError:
         return False
+    except ModuleNotFoundError:
+        print("The 'grp' module is not available on this platform. Group validation will be skipped.")
+        return True
     
 
 def create_cmd_line_args_parser() -> ArgumentParser:
@@ -123,6 +201,12 @@ def create_cmd_line_args_parser() -> ArgumentParser:
         default=4,
         help=f"optional number of worker threads to be used (default = 4, max = {MAX_WORKERS})",
         type=int)
+    parser.add_argument("-d", "--dry-run",
+        dest="dry_run",
+        default=False,
+        action="store_true",
+        help="if specified, the script will not change any ownership or permissions (it will only print what would be changed)"
+    )
 
     return parser
 
@@ -146,6 +230,7 @@ def parse_cmd_line_args() -> Configuration:
         group=params.group,
         permissions=int(params.permissions),
         workers=params.workers,
+        dry_run=params.dry_run,
     )
 
 
@@ -153,20 +238,22 @@ def print_prestart_info(config: Configuration) -> None:
     current_timestamp = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S %Z (%z)")
     print()
     print(f"Going to change ownership & permissions for '{config.root_path}'")
-    print(f"{cpu_count()} CPU core(s) detected, {config.workers} worker thread(s) will be used")
+    print(f"{cpu_count()} CPU core(s) detected, {config.workers} worker process(es) will be used")
     print(f"Start time {current_timestamp}")
     print()
 
 
-def process_request(request: Request) -> Result:
+def process_request(request: Request, dry_run: bool) -> Result:
+    print(f"Going to process directory '{request.path}' (request #{request.seq_no})")
     modified_file_count = 0
     failed_file_count = 0
     modified_dir_count = 0
     failed_dir_count = 0
 
     try:
-        chown(request.path, user=request.user, group=request.group)
-        chmod(request.path, request.permissions)
+        if not dry_run:
+            chown(request.path, user=request.user, group=request.group)
+            chmod(request.path, request.permissions)
         modified_dir_count += 1
     except Exception as e:
         print(f"Error processing directory {request.path}: {e}")
@@ -175,8 +262,9 @@ def process_request(request: Request) -> Result:
     for file_name in request.files:
         file_path = Path(request.path) / file_name
         try:
-            chown(file_path, user=request.user, group=request.group)
-            chmod(file_path, request.permissions)
+            if not dry_run:
+                chown(file_path, user=request.user, group=request.group)
+                chmod(file_path, request.permissions)
             modified_file_count += 1
         except Exception as e:
             print(f"Error processing file {file_path}: {e}")
@@ -192,48 +280,8 @@ def process_request(request: Request) -> Result:
 
 
 def apply_ownership_and_permissions(config: Configuration) -> Summary:
-    exception_count = 0
-    modified_file_count = 0
-    failed_file_count = 0
-    modified_dir_count = 0
-    failed_dir_count = 0
-    sequence = Sequence()
-    future_list = []
-
-    with ThreadPoolExecutor(max_workers=config.workers) as executor:
-        stopwatch = Stopwatch.start()
-        for dir, _, files in walk(config.root_path, topdown=True):
-            request = Request(
-                seq_no=sequence.next_value(),
-                path=dir,
-                files=tuple(files),
-                user=config.user,
-                group=config.group,
-                permissions=config.permissions,
-            )
-            future = executor.submit(process_request, request)
-            future_list.append(future)
-
-    for future in future_list:
-        try:
-            result = future.result()
-            modified_file_count += result.modified_file_count
-            failed_file_count += result.failed_file_count
-            modified_dir_count += result.modified_dir_count
-            failed_dir_count += result.failed_dir_count
-        except Exception as e:
-            print(f"Error in processing: {e}")
-            exception_count += 1
-
-
-    return Summary(
-        overall_duration_millis=stopwatch.elapsed_time_millis(),
-        modified_file_count=modified_file_count,
-        failed_file_count=failed_file_count,
-        modified_dir_count=modified_dir_count,
-        failed_dir_count=failed_dir_count,
-        exception_count=exception_count,
-    )
+    with Prcessor(config) as processor:
+        return processor.process()
 
 
 def print_summary(config: Configuration, summary: Summary) -> None:
@@ -246,6 +294,8 @@ def print_summary(config: Configuration, summary: Summary) -> None:
     print(f"Failed files:         {summary.failed_file_count}")
     print(f"Modified directories: {summary.modified_dir_count}")
     print(f"Failed directories:   {summary.failed_dir_count}")
+    print(f"Exceptions:           {summary.exception_count}")
+    print(f"Dry run:              {'yes' if config.dry_run else 'no'}")
     print()
 
 
